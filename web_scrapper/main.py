25782a275253fae07975711d6215e91194ffa107
AFE99 <Facevedo326@gmail.com>
2022-01-17 12:26:36 -0300

    little changes

--- web_scrapper/main.py
+++ web_scrapper/main.py
@@ -1,15 +1,77 @@
 import argparse
+import csv #Para exportar csv's
+import datetime #Para darle nombre de la fecha actual a mi archivo a exportar
 import logging
+from urllib.error import HTTPError
 logging.basicConfig(level=logging.INFO)
+import re #re=expresiones regulares
 
+#Importo los errores ocupados en _fetch_article
+from requests.exceptions import HTTPError
+from urllib3.exceptions import MaxRetryError
+
+import news_pages_objects as news
 from common import config #Aca leo la configuracion a traves del archivo common
 
 logger = logging.getLogger(__name__)
+#declaro expresiones regulares:
+is_well_formed_link = re.compile(r'^https?://.+/.+$') #Esta expresion regular se fija si el link tiene cierto formato para 'aceptarlo', por ejemplo: https://example.com/hello
+is_root_path = re.compile(r'^/.+$') #Esta expresion 'acepta' los enlaces que hacen referencia a un path, por ej: /some-text
 
 def _news_scraper(news_site_uid):
 	host = config()['news_sites'][news_site_uid]['url']
 
 	logging.info('Beginning scraper for {}'.format(host))
+	homepage = news.HomePage(news_site_uid,host)
+
+	articles = []
+	for link in homepage.article_links:
+		article = _fetch_article(news_site_uid, host, link)
+
+		if article:
+			logger.info('Article fetched!!')
+			articles.append(article)
+			#break
+
+	_save_articles(news_site_uid, articles)	
+
+def _save_articles(news_site_uid, articles):
+	now = datetime.datetime.now().strftime('%Y_%m_%d') #Obtengo fecha y le doy formato de año,mes,dia
+	out_file_name = '{news_site_uid}_{datetime}_articles.csv'.format( #Obtenemos el nombre de archivo a exportar
+		news_site_uid=news_site_uid,
+		datetime=now)
+	csv_headers = list(filter(lambda property: not property.startswith('_'), dir(articles[0])))
+	with open(out_file_name, mode='w+') as f:
+		writer = csv.writer(f)
+		writer.writerow(csv_headers)
+
+		for article in articles:
+			row = [str(getattr(article,prop)) for prop in csv_headers]
+			writer.writerow(row)
+
+def _fetch_article(news_site_uid, host, link):
+	logger.info('Start fetching article at {}'.format(link))
+
+	article = None
+	try:
+		article = news.ArticlePage(news_site_uid, _build_link(host, link))
+	except (HTTPError, MaxRetryError) as e:
+		logger.warning('Error while fetching the article', exc_info=False)
+
+	if article and not article.body:
+		logger.warning('Invalid article. There is no body')
+		return None
+
+	return article	
+
+#Con esta funcion busco construir un link con formato correcto
+def _build_link(host, link): 
+	if is_well_formed_link.match(link): #Ocupo la expresion regular para que se fije si mi link cumple con el formato normal standart
+		return link
+	elif is_root_path.match(link): #Ocupo la expresion regular para ver si estoy en presencia de un link que hace referencia al path y lo completo para obtener el enlace completo
+		return '{}{}'.format(host, link)
+	else: #Si obtuve algo mas que no cumple estas formas, construyo el link improvisado de la siguiente manera:
+		return '{host}/{uri}'.format(host=host, uri=link)
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
---
+++ web_scrapper/news_pages_objects.py
@@ -0,0 +1,52 @@
+import bs4
+import requests
+from common import config
+
+#Declaro una clase PADRE con metodos reutilizables en las diferentes paginas a visitar
+class NewsPage:
+    #Funcion que inicializa la clase
+    def __init__(self, news_site_uid, url): 
+        self._config = config()['news_sites'][news_site_uid]
+        self._queries = self._config['queries']
+        self._html= None
+
+        self._visit(url) #Llamo a la funcion _visit para que visite y parsee la url
+
+    def _select(self, query_string):
+        return self._html.select(query_string)
+
+    def _visit(self, url):
+        # Esto nos va a devolver un objeto response que almacena el codigo fuente de la pagina, con el cual podemos interactuar luego.
+        response = requests.get(url)
+        #Devuelve HTTPError si algo sale mal
+        response.raise_for_status() 
+        #response.text devuelve el codigo html completo
+        self._html = bs4.BeautifulSoup(response.text, 'html.parser') 
+
+class HomePage(NewsPage): #representa la pagina principal de nuestra web, clase HIJA de NewsPage
+    def __init__(self, news_site_uid, url): #Funcion que inicializa la clase
+        super().__init__(news_site_uid, url) #Heredamos de NewsPage la inicializacion de clase
+
+    @property
+    def article_links(self):
+        link_list = [] #Inicializo una list donde voy a guardar los enlaces que halle a articulos
+        for link in self._select(self._queries['homepage_article_links']): #Hago un select de las cosas que encuentre en el codigo html que coincida con lo que especifiqué en homepage_article_links (config.yaml)
+            if link and link.has_attr('href'): #Si lo que encontré tiene un href lo tomo y agrego a la lista de links
+                link_list.append(link)
+            #Ocupo 'set' para que no me devuelva links repetidos en la lista link_list
+            return set(link['href'] for link in link_list) 
+
+class ArticlePage(NewsPage): #representa la interaccion con cada articulo, clase HIJA de NewsPage
+
+    def __init__(self, news_site_uid, url): #Funcion que inicializa la clase
+        super().__init__(news_site_uid, url) #Heredamos de NewsPage la inicializacion de clase
+
+    @property
+    def body(self):
+        result = self._select(self._queries['article_body']) #Hago un select de las cosas que encuentre en el codigo html que coincida con lo que especifiqué en article_body (config.yaml)
+        return result[0].text if len(result) else '' #Cuando ingreso al link de un articulo, el select me devuelve una lista, suponiendo que el primer elemento es el body, lo tomo y lo retorno, si esta vacio, devuelvo nada ''
+
+    @property
+    def title(self):
+        result = self._select(self._queries['article_title']) #Hago un select de las cosas que encuentre en el codigo html que coincida con lo que especifiqué en article_title (config.yaml)
+        return result[0].text if len(result) else '' #Como este metodo se ejecuta por cada link, y el select nos devuelve una lista, tomamos unicamente la primer posicion de la misma, que deberá contener el titulo del articulo, si no existe devuelvo vacio ''.
